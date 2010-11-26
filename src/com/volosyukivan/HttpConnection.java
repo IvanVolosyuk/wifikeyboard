511192bed701edbe8abe02821b4d89d32d951f19
Ivan Volosyuk <Ivan.Volosyuk@gmail.com>
2010-11-27 01:46:54 +1100

    Support for parsing headers and extracting header data.

--- src/com/volosyukivan/HttpConnection.java
+++ src/com/volosyukivan/HttpConnection.java
@@ -4,6 +4,9 @@ import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.SocketChannel;
+import java.util.Arrays;
+
+import android.util.Log;
 
 public abstract class HttpConnection {
   private static final int BUFSIZE = 1024;
@@ -82,7 +85,6 @@ public abstract class HttpConnection {
   private int limit;
   
   private HttpConnectionState httpConnectionState = HttpConnectionState.READ_REQUEST;
-  private HeaderMatcher headerMatcher;
   private boolean isPost;
   
   private ConnectionState newRequestChunk() throws IOException {
@@ -90,7 +92,6 @@ public abstract class HttpConnection {
     data = in.array();
     offset = in.position();
     limit = in.limit();
-    
     exit:
     while (true) {
       switch (httpConnectionState) {
@@ -100,11 +101,15 @@ public abstract class HttpConnection {
           break;
         case LOOK_REQUEST_HEADERS:
           isPost = request[0] == LETTER_P;
-          headerMatcher = lookupHeaderMatcherForRequest();
+          headerMatcher = lookupRequestHandler();
           if (headerMatcher == null)
             httpConnectionState = HttpConnectionState.SKIP_HEADERS;
           else
             httpConnectionState = HttpConnectionState.READ_HEADER;
+          int numHeaders = headerMatcher.patterns.length;
+          for (int i = 0; i < numHeaders; i++) {
+            headerLen[i] = 0;
+          }
           break;
         case READ_HEADER:
           if (offset == limit) break exit;
@@ -135,17 +140,20 @@ public abstract class HttpConnection {
   }
   
   private static final byte LETTER_P = "P".getBytes()[0];
+  private static final byte LETTER_COLUMN = ":".getBytes()[0];
+  private static final byte LETTER_CR = "\n".getBytes()[0];
   
-  byte[] request = new byte[1024];
+  byte[] request = new byte[BUFSIZE];
   int requestLength = 0;
   
   private HttpConnectionState readRequest() {
     int limit = this.limit;
     int requestLength = this.requestLength;
+    byte[] data = this.data;
     
     for (int i = offset; i < limit; i++) {
       byte b = data[i];
-      if (b == 10) {
+      if (b == LETTER_CR) {
         // request end
         this.requestLength = requestLength;
         this.offset = i + 1;
@@ -163,17 +171,179 @@ public abstract class HttpConnection {
     return HttpConnectionState.READ_REQUEST;
   }
   
-  private HttpConnectionState readHeader() {
-    throw new RuntimeException("not implemented");
-//    return HttpConnectionState.READ_HEADER;
+  private HeaderMatcher headerMatcher;
+  private int matchedChars = 0;
+  private int matchedMatcher = 0;
+  
+  enum HeaderState {
+    MATCH_HEADER_NAME,
+    SKIP_HEADER_NAME,
+    STORE_HEADER_DATA,
+    SKIP_HEADER_DATA,
+    NO_MORE_HEADERS
   }
+
+  HeaderState headerState = HeaderState.MATCH_HEADER_NAME;
+  byte[][] header = new byte[2][256];
+  int[] headerLen = new int[2];
+  byte[] currentHeader;
+  int currentHeaderLen;
+
+  private HttpConnectionState readHeader() {
+    while (offset != limit) {
+      switch (headerState) {
+      case MATCH_HEADER_NAME:
+        headerState = matchHeaderName();
+        break;
+      case SKIP_HEADER_NAME:
+        headerState = skipHeaderName();
+        if (headerState == HeaderState.NO_MORE_HEADERS) {
+          headerState = HeaderState.MATCH_HEADER_NAME;
+          return HttpConnectionState.CONSIDER_FORM_DATA;
+        }
+        break;
+      case SKIP_HEADER_DATA:
+        headerState = skipHeaderData();
+        break;
+      case STORE_HEADER_DATA:
+        headerState = storeHeaderData();
+        break;
+      }
+    }
+    return HttpConnectionState.READ_HEADER;
+  }
+  
+  // match bbde
+  // abc   [ matched = 0]
+  // bbce  [ matched=2 skip = 0]
+  // ...
+  // e, skip=0   (failed) 
+  // bbcd, skip=3 (match not possible, skip to next)
+  // bbx, skip=2  (try match)
+  private HeaderState matchHeaderName() {
+    byte[] data = this.data;
+    int limit = this.limit;
+    int matchedChars = this.matchedChars;
+    int matchedMatcher = this.matchedMatcher;
+    
+    HeaderMatcher matcher = headerMatcher;
+    byte[][] patterns = matcher.patterns;
+    int npatterns = patterns.length;
+    byte[] pattern = patterns[matchedMatcher];
+    int i;
+    
+    outer:
+    for (i = offset; i < limit; ) {
+      byte b = data[i];
+      while (true) {
+        if (pattern[matchedChars] == b) {
+          matchedChars++;
+          i++;
+          if (b == LETTER_COLUMN) {
+            // fully matched
+            this.offset = i + 1;
+            this.matchedChars = 0;
+            this.currentHeader = this.header[this.matchedMatcher];
+            this.currentHeaderLen = 0;
+            return HeaderState.STORE_HEADER_DATA;
+          }
+          continue outer;
+        }
+        
+        inner:
+        while (true) {
+          matchedMatcher++;
+          if (matchedMatcher >= npatterns)
+            return HeaderState.SKIP_HEADER_NAME;
+          int similarity = headerMatcher.similarity[matchedMatcher];
+          if (similarity < matchedChars) {
+            this.matchedChars = matchedChars;
+            return HeaderState.SKIP_HEADER_NAME;
+          } else if (similarity > matchedChars) {
+            // will not match, skip this pattern
+            continue inner;
+          } else {
+            // try match this pattern
+            pattern = patterns[matchedMatcher];
+            continue outer;
+          }
+        }
+      }
+    }
+    offset = i;
+    this.matchedChars = matchedChars;
+    this.matchedMatcher = matchedMatcher;
+    return HeaderState.MATCH_HEADER_NAME;
+  }
+  
+  private HeaderState skipHeaderName() {
+    byte[] data = this.data;
+    for (int i = offset; i < limit; i++,matchedChars++) {
+      byte b = data[i];
+      if (b == LETTER_COLUMN) {
+        this.offset = i + 1;
+        return HeaderState.SKIP_HEADER_DATA;
+      } else if (b == LETTER_CR) {
+        if (matchedChars > 1) {
+          throw new ConnectionFailureException("header format error");
+        }
+        this.offset = i + 1;
+        return HeaderState.NO_MORE_HEADERS;        
+      }
+    }
+    this.matchedChars = 0;
+    this.matchedMatcher = 0;
+    return HeaderState.MATCH_HEADER_NAME;
+  }
+  
+  private HeaderState skipHeaderData() {
+    byte[] data = this.data;
+    int limit = this.limit;
+    for (int i = offset; i < limit; i++) {
+      byte b = data[i];
+      if (b == LETTER_CR) {
+        this.offset = i + 1;
+        this.matchedChars = 0;
+        this.matchedMatcher = 0;
+        return HeaderState.MATCH_HEADER_NAME;
+      }
+    }
+    this.offset = limit;
+    return HeaderState.SKIP_HEADER_DATA;
+  }
+  
+  private HeaderState storeHeaderData() {
+    byte[] data = this.data;
+    int limit = this.limit;
+    int currentHeaderLen = this.currentHeaderLen;
+    byte[] currentHeader = this.currentHeader;
+    for (int i = offset; i < limit; i++) {
+      byte b = data[i];
+      if (b == LETTER_CR) {
+//        Log.d("wifikeyboard", "header value = " + new String(currentHeader, 0, currentHeaderLen));
+        this.header[this.matchedMatcher] = currentHeader;
+        this.headerLen[this.matchedMatcher] = currentHeaderLen;
+        this.offset = i + 1;
+        this.matchedChars = 0;
+        this.matchedMatcher = 0;
+        return HeaderState.MATCH_HEADER_NAME;
+      } else {
+        currentHeader[currentHeaderLen++] = b;
+      }
+    }
+    this.offset = limit;
+    this.currentHeaderLen = currentHeaderLen;
+    return HeaderState.STORE_HEADER_DATA;
+  }
+
   
   int skipLineLen = 0;
   private HttpConnectionState skipHeaders() {
+    byte[] data = this.data;
     int skipLineLen = this.skipLineLen;
     int limit = this.limit;
     for (int i = offset; i < limit; i++) {
-      if (data[i] == 10) {
+      if (data[i] == LETTER_CR) {
         if (skipLineLen < 2) {
           // no more headers
           offset = i + 1;
@@ -191,13 +361,42 @@ public abstract class HttpConnection {
     return HttpConnectionState.SKIP_HEADERS;
   }
   
-  private HeaderMatcher lookupHeaderMatcherForRequest() {
-    return null;
+  
+  // FIXME: temporary
+  HeaderMatcher tmpHeaderMatcher = new HeaderMatcher("Accept-Language","Accept-Encoding");
+  
+  private HeaderMatcher lookupRequestHandler() {
+    return tmpHeaderMatcher;
   }
+  
+  // FIXME: dummy form data implementation
+  byte[] formData = new byte[BUFSIZE];
+  int formDataLength = 0;
 
   private HttpConnectionState readFormData() {
-    throw new RuntimeException("not implemented");
-//    return HttpConnectionState.SKIP_HEADERS;
+    byte[] data = this.data;
+    int limit = this.limit;
+    int formDataLength = this.formDataLength;
+    
+    for (int i = offset; i < limit; i++) {
+      byte b = data[i];
+      if (b == LETTER_CR) {
+        // request end
+        this.formDataLength = formDataLength;
+        this.offset = i + 1;
+        Log.d("wifikeyboard", "Form data: " + new String(formData, 0, formDataLength));
+        return HttpConnectionState.EXECUTE_REQUEST;
+      } else {
+        try {
+          formData[formDataLength++] = b;
+        } catch (ArrayIndexOutOfBoundsException e) {
+          throw new ConnectionFailureException("request is too large");
+        }
+      }
+    }
+    this.offset = limit;
+    this.formDataLength = formDataLength;
+    return HttpConnectionState.READ_FORM_DATA;
   }
 
   private ConnectionState executeRequest() throws IOException {
@@ -222,8 +421,30 @@ public abstract class HttpConnection {
   }
   
   class HeaderMatcher {
+    byte[][] patterns;
+    int[] similarity;
+    public HeaderMatcher(String...strings) {
+      Arrays.sort(strings);
+      patterns = new byte[strings.length][];
+      similarity = new int[strings.length];
+      String prevString = null;
+      int nstr = 0;
+      
+      for (String s : strings) {
+        int match = 0;
+        if (prevString != null) {
+          int len = Math.min(s.length(), prevString.length());
+          for (int i = 0; i < len; i++) {
+            if (s.charAt(i) != prevString.charAt(i)) break;
+            match++;
+          }
+        }
+        similarity[nstr] = match;
+        patterns[nstr++] = (s + ":").getBytes();
+        prevString = s;
+      }
+    }
   };
-
 
   private String parseRequest(String line) {
     String[] parts = line.split(" ");
